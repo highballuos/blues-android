523a84d3a41c3bfa3579fded2b7c3053a9de014b
sheele41 <bbang0_0@daum.net>
2021-02-18 16:06:20 +0900

    Splash Lottie

--- app/src/main/java/com/highballuos/blues/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/highballuos/blues/ui/splash/SplashActivity.kt
@@ -1,25 +1,34 @@
 package com.highballuos.blues.ui.splash
 
+import android.animation.Animator
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.highballuos.blues.R
 import com.highballuos.blues.ui.check_setting.CheckSettingActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity: AppCompatActivity() {
-    private val splashDisplayLength = 500L
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val mIntent = Intent(this, CheckSettingActivity::class.java)
-            startActivity(mIntent)
-            finish()
-            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
-        }, splashDisplayLength)
+        splash_lottie_animation.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {}
+
+            override fun onAnimationEnd(animation: Animator?) {
+                val mIntent = Intent(this@SplashActivity, CheckSettingActivity::class.java)
+                startActivity(mIntent)
+                finish()
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {}
+
+            override fun onAnimationRepeat(animation: Animator?) {}
+        })
+
+        splash_lottie_animation.playAnimation()
     }
 
     override fun onBackPressed() {
